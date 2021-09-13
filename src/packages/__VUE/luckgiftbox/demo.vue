7f3b2f6082d9d70e5f262979d2ae54e2df37550c
Ymm0008 <being0008@163.com>
2021-09-13 09:19:59 +0800

    fix: 增加taro文件

--- src/packages/__VUE/luckgiftbox/demo.vue
+++ src/packages/__VUE/luckgiftbox/demo.vue
@@ -2,24 +2,35 @@
   <div class="demo">
     <h2>基础用法</h2>
     <nut-bingo-luckgiftbox
-    
+    :openActive="open" @showPrize="showPrize" :canGift="this.isLocking"
     >
     </nut-bingo-luckgiftbox>
+    <nut-button type="primary" @click="openBox">开礼盒</nut-button>
   </div>
 </template>
 
 <script lang="ts">
+import { ref } from 'vue';
 import { createComponent } from '../../utils/create';
 const { createDemo } = createComponent('luckgiftbox');
 export default createDemo({
   props: {},
   setup() {
-    return {};
+    const open = ref(false);
+    const openBox = () => {
+      open.value = true;
+    }
+    return {
+      open,
+      openBox,
+    };
   }
 });
 </script>
 
 <style lang="scss" scoped>
-.nut-temp {
+.nut-button {
+  display: block;
+  margin: 20px auto;
 }
 </style>
---
+++ src/packages/__VUE/luckscratch/index.taro.vue
@@ -0,0 +1,101 @@
+<template>
+  <div
+    :class="classes"
+    :style="{ height: height + 'px', width: width + 'px' }"
+    ref="luckycardDom"
+    id="luckycardDom"
+  >
+    <div
+      class="lucky-content"
+      v-html="content"
+      :style="{ backgroundColor: backgroundColor, fontSize: fontSize + 'px' }"
+    ></div>
+  </div>
+</template>
+<script lang="ts">
+import { ref, computed, nextTick, onMounted, reactive } from 'vue';
+import { createComponent } from '../../utils/create';
+const { componentName, create } = createComponent('luckscratch');
+import LuckyCard from './luckycard.js';
+export default create({
+  props: {
+    content: {
+      type: String,
+      default: ''
+    },
+    height: {
+      type: [String, Number],
+      default: 50
+    },
+    width: {
+      type: [String, Number],
+      default: 300
+    },
+    coverColor: {
+      type: String,
+      default: '#C5C5C5'
+    },
+    coverImg: {
+      type: String,
+      default: ''
+    },
+    fontSize: {
+      type: [String, Number],
+      default: 20
+    },
+    backgroundColor: {
+      type: String,
+      default: '#FFFFFF'
+    },
+    ratio: {
+      type: [String, Number],
+      default: 0.5
+    }
+  },
+  components: {},
+  emits: ['click', 'open'],
+
+  setup(props, { emit }) {
+    const luckycardDom = ref<HTMLElement | null>(null);
+    const classes = computed(() => {
+      const prefixCls = componentName;
+      return {
+        [prefixCls]: true,
+      };
+    });
+    const state:any = reactive({
+      luckcard: null
+    });
+    const clearCover = () => {
+      state.luckcard.clearCover();
+    };
+    onMounted(() => {
+      nextTick(() => {
+        state.luckcard = LuckyCard(
+          {
+            scratchDiv: luckycardDom.value,
+            coverColor: props.coverColor,
+            coverImg: props.coverImg,
+            ratio: Number(props.ratio),
+            callback: function () {
+              //console.log(this);
+              clearCover();
+              emit('open', this);
+            }
+          },
+          () => {}
+        ) as any;
+      });
+    });
+
+    return {
+      classes,
+      luckycardDom
+    };
+  }
+});
+</script>
+
+<style lang="scss">
+@import 'index.scss';
+</style>
