5dd46d482c4cd89e5399e9295f70f4fec2b75cab
Ymm0008 <being0008@163.com>
2021-11-19 18:06:28 +0800

    fix: 滚动抽奖优化

--- tsc/test/src/packages/nutui.vue.js
+++ tsc/test/src/packages/nutui.vue.js
@@ -5,8 +5,10 @@ import Luckscratch from './__VUE/luckscratch/index.vue';
 import Luckgiftbox from './__VUE/luckgiftbox/index.vue';
 import lottoRoll from './__VUE/lottoroll/index.vue';
 import Luckeggs from './__VUE/luckeggs/index.vue';
+import Luckshake from './__VUE/luckshake/index.vue';
+import Dollmachine from './__VUE/dollmachine/index.vue';
 function install(app) {
-    const packages = [Luckdraw, Luckmarquee, Lucknine, Luckscratch, Luckgiftbox, lottoRoll, Luckeggs, Luckshake];
+    const packages = [Luckdraw, Luckmarquee, Lucknine, Luckscratch, Luckgiftbox, lottoRoll, Luckeggs, Luckshake, Dollmachine];
     packages.forEach((item) => {
         if (item.install) {
             app.use(item);
@@ -16,5 +18,5 @@ function install(app) {
         }
     });
 }
-export { install, Luckdraw, Luckmarquee, Lucknine, Luckscratch, Luckgiftbox, lottoRoll, Luckeggs, Luckshake  };
+export { install, Luckdraw, Luckmarquee, Lucknine, Luckscratch, Luckgiftbox, lottoRoll, Luckeggs, Luckshake, Dollmachine };
 export default { install, version: '0.0.1' };
