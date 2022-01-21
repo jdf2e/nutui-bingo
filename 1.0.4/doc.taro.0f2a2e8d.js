86158195b387d03989c38ef4afeeefbd06298969
Ymm0008 <Ymm0008@users.noreply.github.com>
2022-01-21 08:38:05 +0000

    deploy: 835ebcea245767575b08a7db4b4f34a8ced4828b

---
+++ 1.0.4/doc.taro.0f2a2e8d.js
@@ -0,0 +1,1 @@
+import{e as a,o as s,g as l}from"./vendor.dec5a0ae.js";const o={class:"markdown-body"},d=[l("h1",null,"luckycard",-1),l("h3",null,"介绍",-1),l("p",null,"由于 taro 对原生 canvas 的转换未完成。暂时不支持该组件转小程序的应用",-1)],e={setup:l=>(l,e)=>(s(),a("div",o,d))};export{e as default};
