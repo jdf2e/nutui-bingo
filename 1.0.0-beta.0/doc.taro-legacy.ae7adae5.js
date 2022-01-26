ffad7900639503fd388b6b5d05ea0adb27bb8cd0
Ymm0008 <Ymm0008@users.noreply.github.com>
2022-01-26 08:23:09 +0000

    deploy: 942bbc92aaae95817b282eada208e0807a270f4d

--- 1.0.0-beta.0/doc.taro-legacy.ae7adae5.js
+++
@@ -1,1 +0,0 @@
-System.register(["./vendor-legacy.10380435.js"],(function(e){"use strict";var t,n,s;return{setters:[function(e){t=e.e,n=e.o,s=e.g}],execute:function(){const u={class:"markdown-body"},c=[s("h1",null,"luckycard",-1),s("h3",null,"介绍",-1),s("p",null,"由于 taro 对原生 canvas 的转换未完成。暂时不支持该组件转小程序的应用",-1)];e("default",{setup:e=>(e,s)=>(n(),t("div",u,c))})}}}));
