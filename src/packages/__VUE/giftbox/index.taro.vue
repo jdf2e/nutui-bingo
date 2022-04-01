6b111d5474829b8eadf227a8be8a941e77a0d459
Ymm0008 <being0008@163.com>
2022-04-01 11:28:44 +0800

    fix: 开礼盒小程序适配优化

--- src/packages/__VUE/giftbox/index.taro.vue
+++ src/packages/__VUE/giftbox/index.taro.vue
@@ -1,83 +1,98 @@
 <template>
   <view class="giftbox-wraper">
     <view :class="classes" @click="handleClick">
-      <view id="giftAnimate" class="gBox gift-box-1" :class="{'gift-box-1-open': openActive}"></view>
+      <view
+        id="giftAnimate"
+        class="gBox gift-box-1"
+        :class="{ 'gift-box-1-open': openActive }"
+      ></view>
       <view class="gBox gift-box-2"></view>
-      <view class="gBox gift-box-3" :class="{'gift-box-3-open': openActive}"></view>
+      <view
+        class="gBox gift-box-3"
+        :class="{ 'gift-box-3-open': openActive }"
+      ></view>
     </view>
   </view>
 </template>
 <script lang="ts">
-import { onMounted, ref, computed } from 'vue';
-import { createComponent } from '../../utils/create';
-const { componentName, create } = createComponent('giftbox');
+import { onMounted, ref, computed } from "vue";
+import { createComponent } from "../../utils/create";
+const { componentName, create } = createComponent("giftbox");
+import Taro, {
+  eventCenter,
+  getCurrentInstance as getCurrentInstanceTaro,
+} from "@tarojs/taro";
 
 export default create({
   props: {
     initPrize: {
       type: Boolean,
-      default: true
-    }
+      default: true,
+    },
   },
-  emits: ['start-turns', 'end-turns'],
+  emits: ["start-turns", "end-turns"],
 
   setup(props, { emit }) {
     const classes = computed(() => {
       const prefixCls = componentName;
       return {
         [prefixCls]: true,
-        "gift-box": true
+        "gift-box": true,
       };
     });
 
+    const removeListen = () => {
+      const gift: HTMLElement = document.getElementById(
+        "giftAnimate"
+      ) as HTMLElement;
+      gift.removeEventListener("webkitTransitionEnd", function () {});
+    };
+
+    const gift = () => {
+      let transitionFlag = true;
+      const gift: HTMLElement = document.getElementById(
+        "giftAnimate"
+      ) as HTMLElement;
+      gift.addEventListener("webkitTransitionEnd", function (e) {
+        if (e.target === e.currentTarget && transitionFlag) {
+          transitionFlag = false;
+          emit("end-turns");
+          removeListen();
+        }
+      });
+    };
+
     const openActive = ref(false);
 
-    const handleClick = (event: Event) => {
+    const handleClick = () => {
       if (openActive.value) {
-        return false
+        return false;
       }
-      emit('start-turns');
+      emit("start-turns");
       openActive.value = true;
       gift();
     };
 
     const init = () => {
       openActive.value = false;
-    }
+    };
 
-    // onMounted(() => {
-    //   setTimeout(() => {
-    //   openActive.value = false;
-    //   }, 400);
-    // })
-
-    const gift = () => {
-      let transitionFlag = true;
-      let gift: HTMLElement = document.getElementById('giftAnimate') as HTMLElement;
-      gift.addEventListener("webkitTransitionEnd", function(e){
-        if(e.target === e.currentTarget && transitionFlag) {
-          transitionFlag = false;
-          emit('end-turns');
-          removeListen();
-        }
+    onMounted(() => {
+      eventCenter.once((getCurrentInstanceTaro() as any).router.onReady, () => {
+        gift();
       });
-    }
-    
-    const removeListen = () => {
-      let gift: HTMLElement = document.getElementById('giftAnimate') as HTMLElement;
-      gift.removeEventListener("webkitTransitionEnd", function(){});
-    }
+    });
 
     return {
       classes,
       init,
       openActive,
-      handleClick
+      handleClick,
     };
-  }
+  },
 });
 </script>
 
 <style lang="scss">
-@import 'index.scss';
+@import "index.scss";
 </style>
